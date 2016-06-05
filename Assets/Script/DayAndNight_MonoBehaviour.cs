523cfa8b9e9a614c23f6b9945a78c283f6f7e783
chamto <anrudco@gmail.com>
2016-06-06 03:51:08 +0900

    [ 낮과밤 배경화면 변하는 효과 추가 ]

---
+++ Assets/Script/DayAndNight_MonoBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayAndNight_MonoBehaviour : MonoBehaviour 
+{
+
+	public Color _outColor; 
+	public Color _startColor = Color.white;
+	public Color _middleColor = Color.blue;
+	public Color _endColor = Color.black;
+
+	public float _dayTime = 20;
+	public float _nightTime = 20;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		_outColor = _startColor;
+
+	}
+
+	private float _Update_elapsedTime = 0;
+	private float _Update_timeOrientation = 1;
+	private Color _Update_tempColor;
+	void Update () 
+	{
+		_Update_elapsedTime += Time.deltaTime * _Update_timeOrientation;
+
+		//day -> night
+		if (_Update_elapsedTime <= 0)
+		{
+			_Update_timeOrientation = 1;
+			_Update_elapsedTime = 0;
+		}
+
+		//day <- night
+		if (_Update_elapsedTime > _dayTime + _nightTime) 
+		{
+			_Update_timeOrientation = -1;
+		}
+
+		if (1 == _Update_timeOrientation) 
+		{
+			//day -> night
+			if (0 <= _Update_elapsedTime && _Update_elapsedTime <= _dayTime) 
+			{
+				_outColor = Color.Lerp (_startColor, _middleColor, _Update_elapsedTime / _dayTime);
+				_Update_tempColor = _outColor;
+			} else 
+			{
+				_outColor = Color.Lerp (_Update_tempColor, _endColor, (_Update_elapsedTime - _dayTime) / _nightTime);
+			}
+		} else 
+		{
+			//day <- night
+			if (0 <= _Update_elapsedTime && _Update_elapsedTime <= _dayTime) {
+				_outColor = Color.Lerp (_startColor, _Update_tempColor, _Update_elapsedTime / _dayTime);
+			} else 
+			{
+				_outColor = Color.Lerp (_middleColor, _endColor, (_Update_elapsedTime - _dayTime) / _nightTime);
+				_Update_tempColor = _outColor;
+			}
+		}
+
+
+		Camera.main.backgroundColor = _outColor;
+	}
+}
